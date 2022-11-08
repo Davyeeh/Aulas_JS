f03f912de4de374aba79d1d9e742ebe04ae6570d
Davi Araújo <ds818860@gmail.com>
2022-11-08 16:05:07 -0300

    novo

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 12
+console.log(`A velocidade do seu carro é ${vel}km/h`)
+if (vel > 60) { //Condição simples
+    console.log('Sua velocidade está acima do limite, MULTADO!')
+}
+console.log('Dirija sempre usando o cinto de segurança!')
