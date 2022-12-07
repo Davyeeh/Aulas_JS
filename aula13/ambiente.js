827acbdf630fe94a27b1e90f76f84b8185ed630e
Davi Araújo <ds818860@gmail.com>
2022-12-07 14:55:33 -0300

    New

---
+++ aula13/ambiente.js
@@ -0,0 +1,25 @@
+var c = 1
+
+do{ //modo 2
+ console.log(`Passo ${c}`)
+ c++
+} while (c <= 6)
+
+
+/* 
+    modo 1 (mais comum)
+var c = 1
+
+while (c <= 6){
+ console.log(`Passo ${c}`)
+ c++
+}
+*/ 
+
+
+/* Pior solução
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+*/
