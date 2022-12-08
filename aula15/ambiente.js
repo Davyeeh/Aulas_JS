7398873a99d25e0ee342b75087f00e4bc47e09f0
Davi Araújo <ds818860@gmail.com>
2022-12-08 14:12:21 -0300

    new update

--- aula15/ambiente.js
+++ aula15/ambiente.js
@@ -1,16 +1,19 @@
+
+//VARIÁVEL COMPOSTA
+
 let num = [5, 2, 8, 9, 3]
-num[5] = 6
-num.push(1)
+num[5] = 6 //add um número a varialvel composta e pode tbm subistituir um
+num.push(1) //add um número a varialvel composta
 //num.length
 
-num.sort()
-console.log(num)
-console.log(`O vetor tem ${num.length} posições`)
-console.log(`O primeiro valor do vetor é ${num[0]}`)
+num.sort() //Coloca os numeros em ordem crescente
+console.log(num) //Mostra os valores da variável
+console.log(`O vetor tem ${num.length} posições`) //.length mostra o tamanho da variável
+console.log(`O primeiro valor do vetor é ${num[0]}`) //mostra o valor de um vetor usando uma posição
 
-let pos = num.indexOf(8)
-if(pos == -1){
-    console.log(`O valor não foi encontrado`)
+let pos = num.indexOf(8) //busca uma chave de um valor
+if(pos == -1){ //caso o valor n exista ele retorna -1
+    console.log(`O valor não foi encontrado`) //mostra essa frase caso o valor não exista
 }else{
-console.log(`O valor esta na posição ${pos}`)
+console.log(`O valor esta na posição ${pos}`) //mostra essa caso o valor exista
 }
--- aula15/vetor_na_tela.js
+++ aula15/vetor_na_tela.js
@@ -1,18 +1,20 @@
-let valores = [8, 1, 7, 4, 2, 9]
+let valores = [8, 1, 7, 4, 2, 9] //variável composta
 
 //console.log(valores)
 /*
-console.log(valores[0])
+console.log(valores[0]) //*pior* maneira de mostrar o valor de uma posição
 console.log(valores[1])
 console.log(valores[2])
 console.log(valores[3])
 console.log(valores[4])
-console.log(valores[5])
+console.log(valores[5]) 
 */
-/*for(let pos=0; pos < valores.length; pos++){
-    console.log(`A posição ${pos} tem o valor ${valores[pos]}`)
+
+/*
+for(let pos=0; pos < valores.length; pos++){
+    console.log(`A posição ${pos} tem o valor ${valores[pos]}`) //mostra o valor de cada posição
 }*/
 valores.sort()
 for(let pos in valores){
-    console.log(`A posição ${pos} tem o valor ${valores[pos]}`)
+    console.log(`A posição ${pos} tem o valor ${valores[pos]}`) //*melhor* maneira de informar o valor de cada posição
 }
