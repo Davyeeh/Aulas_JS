dce268bc9a4e7c13db134c0dc24df27c98e06ef8
Davi Araújo <ds818860@gmail.com>
2022-12-23 17:12:19 -0300

    Update script.js

--- sozinho/tabuada/script.js
+++ sozinho/tabuada/script.js
@@ -3,14 +3,14 @@ function tabuada(){
     let tab = document.getElementById('seltab');
 
     if(num.value.length == 0){
-        window.alert('Por favor, insira um número')
+        window.alert('Por favor, insira um número');
     }else{
         let n = Number(num.value);
         let c = 1;
-        tab.innerHTML = ''
+        tab.innerHTML = '';
         while(c <= 10) {
-            let item = document.createElement('option')
-            item.text = `${n} x ${c} = ${n*c}`
+            let item = document.createElement('option');
+            item.text = `${n} x ${c} = ${n*c}`;
             item.value = `tab${c}`;
             tab.appendChild(item);
             c++
